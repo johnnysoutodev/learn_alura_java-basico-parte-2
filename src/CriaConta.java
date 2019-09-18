46104e7e63fe97345072b7a7ac3555355e0956b6
Johnny Souto <johnny.souto@icloud.com>
2019-09-17 22:37:41 -0300

    Update CriaConta.java

--- src/CriaConta.java
+++ src/CriaConta.java
@@ -4,6 +4,9 @@ public class CriaConta {
 	public static void main(String[] args) {
 		Conta primeiraConta = new Conta();
 		primeiraConta.saldo = 200;
+		
+		primeiraConta.saldo +=100;
+		
 		System.out.println("Meu saldo é: " + primeiraConta.saldo);
 	}
 
