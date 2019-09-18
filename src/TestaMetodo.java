d8d0a46c63ba2542dacae3837bf024e087acbd34
Johnny Souto <johnny.souto@icloud.com>
2019-09-18 00:44:39 -0300

    updates

--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -9,6 +9,10 @@ public class TestaMetodo {
 		conta.deposita(1000);
 		
 		System.out.println("Meu saldo é: " + conta.saldo);
+		
+		boolean conseguiuRetirar = conta.saca(25);
+		System.out.println(conseguiuRetirar);
+		System.out.println("Meu novo saldo é: " + conta.saldo);
 	}
 
 }
