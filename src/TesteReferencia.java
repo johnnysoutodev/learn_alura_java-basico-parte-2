3cb2bf72998fcd2bb81fc83138224cb4c083661a
Johnny Souto <johnny.souto@icloud.com>
2019-09-17 23:22:20 -0300

    Update TesteReferencia.java

--- src/TesteReferencia.java
+++ src/TesteReferencia.java
@@ -3,5 +3,15 @@ public class TesteReferencia{
 	public static void main(String[] args) {
 		Conta primeiraConta = new Conta();
 		primeiraConta.saldo = 300;
+		
+		System.out.println("Saldo da primeira conta: " + primeiraConta.saldo);
+		
+		Conta segundaConta = primeiraConta;
+		
+		System.out.println("saldo da segunda conta: " + segundaConta.saldo);
+		
+		if (primeiraConta == segundaConta) {
+			System.out.println("Sao a mesma coisa");
+		}
 	}
 }
