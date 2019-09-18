6257601d9e7293ec4ef77407cb4a802f8b192555
Johnny Souto <johnny.souto@icloud.com>
2019-09-18 19:27:35 -0300

    Create TestaPrograma.java

---
+++ outro/src/TestaPrograma.java
@@ -0,0 +1,15 @@
+
+public class TestaPrograma {
+	
+	public static void main(String[] args) {
+		
+		Conta contaDaMarcela = new Conta();
+		
+		contaDaMarcela.titular = new Cliente();
+		
+		contaDaMarcela.titular.nome = "Marcela";
+		
+		System.out.println(contaDaMarcela.titular.nome);
+	}
+
+}
